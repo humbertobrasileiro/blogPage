485e266580cc9b70df92f3c9b9cd8e9c0a1e3fb9
Carlos Humberto Brasileiro Toscano <humbertobtoscano@gmail.com>
2021-08-18 20:54:51 -0300

    atualizando husky

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -11,6 +11,86 @@ describe('<Footer />', () => {
         font-size: 2.4rem;
       }
 
+      .c2 p {
+        margin: 4.0rem 0;
+      }
+
+      .c2 a,
+      .c2 a:visited,
+      .c2 a:link {
+        color: #dc143c;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        -webkit-transition: all 300ms ease-in-out;
+        transition: all 300ms ease-in-out;
+      }
+
+      .c2 a:hover {
+        -webkit-filter: brightness(50%);
+        filter: brightness(50%);
+      }
+
+      .c2 pre {
+        background: #000000;
+        padding: 2.4rem;
+        font-family: monospace;
+        color: #FFFFFF;
+        margin: 4.0rem 0;
+        width: 100%;
+        overflow-x: auto;
+        font-size: 1.6rem;
+      }
+
+      .c2 img {
+        max-width: 100%;
+      }
+
+      .c2 .image {
+        background: #DDDDDD;
+        line-height: 0;
+        margin: 4.0rem 0;
+      }
+
+      .c2 .image figcaption {
+        font-size: 1.6rem;
+        padding: 1.6rem;
+        text-align: center;
+        line-height: 1.3;
+      }
+
+      .c2 .image-style-side {
+        max-width: 50%;
+        float: right;
+        margin: 2.4rem;
+      }
+
+      .c2 hr {
+        border: none;
+        border-bottom: 0.1rem solid #DDDDDD;
+      }
+
+      .c2 ul,
+      .c2 ol {
+        margin: 4.0rem;
+      }
+
+      .c2 .table {
+        width: 100%;
+        overflow: hidden;
+        overflow-x: auto;
+      }
+
+      .c2 table {
+        width: 100%;
+        border-collapse: collapse;
+      }
+
+      .c2 table td,
+      .c2 table th {
+        padding: 1.6rem;
+        border: 0.1rem solid #DDDDDD;
+      }
+
       .c0 {
         text-align: center;
         border-top: 0.1rem solid #DDDDDD;
@@ -24,6 +104,18 @@ describe('<Footer />', () => {
 
       .c0 .c1 {
         font-size: 1.6rem;
+      }
+
+      @media (max-width:768px) {
+        .c2 {
+          font-size: 2rem;
+        }
+
+        .c2 .image-style-side {
+          max-width: 100%;
+          float: none;
+          margin: 0;
+        }
       }
 
       <div>
