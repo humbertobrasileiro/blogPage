5272694faac11de4d20e134447f407b2e5d13c69
Carlos Humberto Brasileiro Toscano <humbertobtoscano@gmail.com>
2021-08-26 14:20:12 -0300

    postsTemplate

---
+++ src/templates/PostsTemplate/index.tsx
@@ -0,0 +1,17 @@
+import { SettingsStrapi } from '../../shared-typed/settings-strapi';
+import { PostStrapi } from '../../shared-typed/post-strapi';
+import { BaseTemplate } from '../Base';
+import { PostGrid } from '../../components/PostGrid';
+
+export type PostsTemplateProps = {
+  settings: SettingsStrapi;
+  posts?: PostStrapi[];
+};
+
+export const PostsTemplate = ({ settings, posts = [] }: PostsTemplateProps) => {
+  return (
+    <BaseTemplate settings={settings}>
+      <PostGrid posts={posts} />
+    </BaseTemplate>
+  );
+};
