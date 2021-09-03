ed46e181be2a71b7dcabe1a3cf04b7bd738fead9
Carlos Humberto Brasileiro Toscano <humbertobtoscano@gmail.com>
2021-09-03 10:18:28 -0300

    deploy vercel - disqus

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -16,7 +16,7 @@ export const Comments = ({ id, slug, title, allowComments }: CommentsProps) => {
       <DiscussionEmbed
         shortname="blog-humberto-brasileiro"
         config={{
-          url: `/post/${slug}`,
+          url: `https://blog-humberto.systeccg.com.br/post/${slug}`,
           identifier: id,
           title: title,
           language: 'pt_BR',
